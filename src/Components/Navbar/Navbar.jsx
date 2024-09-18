d365b97caa3351211fa82b74a17b31e0b857ff92
Dharanikumar K <kdharanstarflit@gmail.com>
2024-09-18 15:55:28 +0530

    Navbar Changed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,53 +10,62 @@ const Navbar = () => {
   const closeMenu = () => setClick(false);
 
   return (
-    <section>
-      <div className="topbar">
-        <div className="container text-center py-3 d-flex flex-wrap justify-content-center justify-content-xl-between align-items-center">
-          <div className="title">
-            <h4 className="mb-2">Education and Employment Verification Process</h4>
-          </div>
-          <div className="top-btn">
-            <button className="me-2">Help</button>
-            <button>Languages</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="header">
-        {/* <div className="container"> */}
-        <nav className="navbar py-1">
-          <Link to="/" onClick={closeMenu} className="logo">
-            <img src={Logo} alt="Logo" />
-          </Link>
-          <div className="hamburger" onClick={handleClick}>
-            {click ? <FaTimes size={30} /> : <FaBars size={30} />}
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" onClick={closeMenu}>Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" onClick={closeMenu}>About Us</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" onClick={closeMenu}>Our Services</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/why" onClick={closeMenu}>Why EAEVP?</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" onClick={closeMenu}>Contact Us</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" onClick={closeMenu}>
-                <button type="submit">LOGIN</button>
+    <section className="header">
+      <div className="container">
+        <div className="row">
+          <div className="d-flex flex-wrap justify-content-center align-items-center">
+            <div className="col-sm-12 col-lg-2 text-end py-2">
+              <Link to="/" onClick={closeMenu} className="logo">
+                <img src={Logo} alt="Logo" />
               </Link>
-            </li>
-          </ul>
-        </nav>
-        {/* </div> */}
+            </div>
+            <div className="col-10">
+              <div className="hamburger" onClick={handleClick}>
+                {click ? <FaTimes size={30} /> : <FaBars size={30} />}
+              </div>
+              <nav className="navbar d-flex justify-content-end">
 
+                <div className="topbar py-3 px-4 d-flex flex-wrap justify-content-center align-items-center">
+                  <div className="title">
+                    <h4 className="me-lg-5 me-sm-0 mb-2">Education and Employment Verification Process</h4>
+                  </div>
+                  <div className="top-btn">
+                    <button className="me-2">Help</button>
+                    <button>Languages</button>
+                  </div>
+                </div>
+
+
+
+                <div>
+                  <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                      <Link to="/" onClick={closeMenu}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/about" onClick={closeMenu}>About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/services" onClick={closeMenu}>Our Services</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/why" onClick={closeMenu}>Why EAEVP?</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/contact" onClick={closeMenu}>Contact Us</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/login" onClick={closeMenu}>
+                        <button type="submit">LOGIN</button>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+
+        </div>
       </div>
     </section>
   );
